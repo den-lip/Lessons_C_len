8ebfe82ff89433297773d9896275cadb07aea761
Anastasia <denisovaanastasia202@gmail.com>
2023-06-26 12:00:16 +0300

    Создали программу по заданию 1 семинара

---
+++ Exercise_one.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число  ");
+string number = Console.ReadLine ();
+int number1 = Convert.ToInt32 (number);
+int square;
+square = number1*number1;
+Console.WriteLine ("Квадрат числа " + number + " равен " + square);
+
+
+
+
+
