67275d4e9480577bc6cea0d07324189dfe19c797
Anastasia <denisovaanastasia202@gmail.com>
2023-06-29 16:55:23 +0300

    Создали программу с нахождением наименьшего и наибольшего числа

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
-﻿Console.WriteLine("Попытайтесь угадать число от 3 до 7");
-Console.WriteLine("Введите предположительное число ");
-string number = Console.ReadLine();
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-if (number == "6")
+Console.WriteLine("Введите первое число");
+string num1 = Console.ReadLine();
+int number1 = Convert.ToInt32(num1);
+Console.WriteLine ("Введите второе число");
+string num2 = Console.ReadLine();
+int number2 = Convert.ToInt32(num2);
+if ( number1 < number2)
 {
-    Console.WriteLine ("Вы угадали!");
-
+    Console.WriteLine("Число " + number1 + " меньше числа " + number2);
 }
-else 
+else
 {
-Console.WriteLine ("Увы, вы не угадали");
-Console.WriteLine ("Верное число 6");
-
+    Console.WriteLine ("Число " + number1 + " больше числа " + number2);
 }
+
+
+
+
+
+
+
+
 
