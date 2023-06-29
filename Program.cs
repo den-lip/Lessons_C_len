4abc091578decba5dddb8c51e91e6ed8e7e8d1ff
den-lip <136602716+den-lip@users.noreply.github.com>
2023-06-29 15:27:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+// Создадим туже программу, но с рандомом
+
+int numberA = new Random().Next(1,15);
+int numberB = new Random().Next(1,5);
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
+Console.WriteLine(numberA+numberB);
