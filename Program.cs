ff71bf40b0a13de5067ccbdf370cda5509c7b0bc
Anastasia <denisovaanastasia202@gmail.com>
2023-06-29 15:17:52 +0300

    Создали программу с суммой рандомных чисел

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿int numA = 4;
-int numB = 8;
-Console.WriteLine(numA+numB);
+﻿
+// Создадим туже программу, но с рандомом
 
+int numberA = new Random().Next(1,15);
+int numberB = new Random().Next(1,5);
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
+Console.WriteLine(numberA+numberB);
